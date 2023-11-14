1508738eefadc83aec0f2aef4e9c3a35ab76af8c
Abdurauf2309 <a.qarshiboyev@newuu.uz>
2023-11-14 23:53:15 +0500

    all is done!

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,9 +1,18 @@
 #include <string>
 #include <sstream>
+using namespace std;
 
-std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
-
-    // make use of control flow statements
-    // return result
+string problemSolution4(const string &macAddress) {
+    string result;
+    int firstOct;
+    stringstream sso(macAddress);
+    sso >> hex >> firstOct;
+    if (macAddress=="FF:FF:FF:FF:FF:FF"){
+        result = "Broadcast";
+    }
+    if (firstOct%2==0){
+        result = "Unicast";
+    }else
+        result = "Multicast";
+    return result;
 }
